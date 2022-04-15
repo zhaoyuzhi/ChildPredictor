3d36fa87cc7679bc4da602d885e4d7aea4808172
Yuzhi ZHAO <yzzhao2-c@my.cityu.edu.hk>
2022-04-15 15:07:18 +0800

    Add files via upload

--- code/babymapping_1219/run.sh
+++ code/babymapping_1219/run.sh
@@ -2,7 +2,6 @@
 #MV2_USE_CUDA=1 MV2_ENABLE_AFFINITY=0 MV2_SMP_USE_CMA=0
 #SBATCH -p Pixel
 #SBATCH --gres=gpu:4
-#SBATCH --nodelist=SH-IDC2-172-20-20-85
-#SBATCH --job-name=multigt-ijcai21
+#SBATCH --job-name=multigt
 #SBATCH -o ./logs/%j.txt
 srun --mpi=pmi2 sh ./yaml/script/Mapping_Xencoder_full_ProGAN_GAN_MSGAN_ACGAN_deepArch_multi-gt_v4.sh
